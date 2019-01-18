e80d2f34a9ee0c0210c017025da52c1888edaabe
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-01-18 14:16:21 +0100

    Add some content to site

--- src/App.js
+++ src/App.js
@@ -3,7 +3,79 @@ import './App.css'
 
 class App extends Component {
   render() {
-    return <div className="App">Effektivisere Akutten med Droner</div>
+    return (
+      <div className="App">
+        <h1>Effektivisere Akutten med Droner</h1>
+        <p>
+          Dette er en alternativ presentasjonsform i EiT fra gruppe 5 i
+          landsbyen for Fremtidens Smarte Sykehus.
+        </p>
+
+        <p>Nettsiden inneholder:</p>
+        <ol>
+          <li>
+            <a href="#demo">Interaktiv demo av løsning</a>
+          </li>
+          <li>
+            <a href="#description">
+              Beskrivelse av problemet med effektivitet i akutt
+            </a>
+          </li>
+          <li>
+            <a href="#solution">Løsning med droner</a>
+          </li>
+        </ol>
+
+        <h2 id="demo">Interaktiv demo av løsning</h2>
+        <button id="play">Start</button>
+
+        <h2 id="description">
+          Beskrivelse av problemet med effektivitet i akutt
+        </h2>
+        <p>
+          <i>
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum
+            Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem
+            ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum
+            Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem
+            ipsumLorem ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem
+            ipsum Lorem ipsum
+          </i>
+        </p>
+        <p>
+          <i>
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum
+            Lorem ipsumLipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum
+          </i>
+        </p>
+
+        <h2 id="solution">Løsning med droner</h2>
+        <p>
+          <i>
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum
+            Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem
+            ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum
+            Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem
+            ipsumLorem ipsumLorem ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem
+            ipsum Lorem ipsum
+          </i>
+        </p>
+        <p>
+          <i>
+            Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem
+            ipsumLorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem
+            ipsum Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum
+            Lorem ipsumLipsum Lorem ipsum Lorem ipsumLorem ipsum Lorem ipsum
+          </i>
+        </p>
+      </div>
+    )
   }
 }
 
