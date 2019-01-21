7d0c9749f218c6cf865c54af646b01d13355753a
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-01-21 10:41:18 +0100

    Updates

---
+++ precache-manifest.4904f1621cea6e9eb545b7e7ec172d39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac2fdd98dd52cdd5b651",
+    "url": "/static/js/main.ac2fdd98.chunk.js"
+  },
+  {
+    "revision": "7ff15490990ead39882c",
+    "url": "/static/js/1.7ff15490.chunk.js"
+  },
+  {
+    "revision": "ac2fdd98dd52cdd5b651",
+    "url": "/static/css/main.d8b3b7f0.chunk.css"
+  },
+  {
+    "revision": "fdee105a03b169c46c87ed03132e5fcc",
+    "url": "/index.html"
+  }
+];
