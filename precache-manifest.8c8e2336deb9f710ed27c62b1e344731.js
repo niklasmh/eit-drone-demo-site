64c516b5034d6d3cc8b9fd9d22033aec9151087e
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-01-23 13:37:56 +0100

    Updates

---
+++ precache-manifest.8c8e2336deb9f710ed27c62b1e344731.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "42ae42ed41b0619912c6",
+    "url": "/static/js/main.42ae42ed.chunk.js"
+  },
+  {
+    "revision": "9f3d682c55ccfbbfbff8",
+    "url": "/static/js/1.9f3d682c.chunk.js"
+  },
+  {
+    "revision": "42ae42ed41b0619912c6",
+    "url": "/static/css/main.7f6e105c.chunk.css"
+  },
+  {
+    "revision": "2a0ebd4bdd365d1a8e7f7f6bf52610d7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f2dcb03f34495b8e797fc2eafd708e8.js"
+  "/precache-manifest.8c8e2336deb9f710ed27c62b1e344731.js"
 );
 
 workbox.clientsClaim();
