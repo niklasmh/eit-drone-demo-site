a1799be518f359d73fb2cb75fd2e39d166abdf47
Niklas Molnes Hole <nikasmh@hotmail.com>
2019-01-24 10:24:01 +0100

    Add more content

--- src/App.js
+++ src/App.js
@@ -86,7 +86,7 @@ class App extends Component {
               src={process.env.PUBLIC_URL + 'logo.svg'}
             />
           </div>
-          <h1>Effektivisere Akutten med Droner</h1>
+          <h1>Fremtidens akuttmottak</h1>
           <p>
             Dette er en alternativ presentasjonsform i EiT fra gruppe 5 i
             landsbyen for Fremtidens Smarte Sykehus.
@@ -146,18 +146,18 @@ class App extends Component {
         </Page>
 
         <Page color="white" page="tidslinje" refCallback={this.refPage}>
-          <h2 className="no-bottom">Droner i behandling av</h2>
+          <h2 className="no-bottom">eDrone i behandling av</h2>
           <h1 className="no-top no-bottom">Opioidoverdoser</h1>
           <p>Pårørende oppdager en overdose</p>
           <FadeInBlock side="right" title="Symptomer">
-            Opioidoverdoser kjennetegnes av nedsatt bevissthet og nedsatt
+            Opioidoverdoser kjennetegnes av nedsatt bevissthet og nedsatt.
             respirasjon
           </FadeInBlock>
           <FadeInBlock side="left" title="En livstruende tilstand">
-            Ved en overdose med opioider slutter pasienten å puste
+            Ved en overdose med opioider slutter pasienten å puste.
           </FadeInBlock>
           <FadeInBlock side="right" title="263 overdosedødsfall i året">
-            I Norge dør det i snitt 263 personer av overdoser hvert år
+            I Norge dør det i snitt 263 personer av overdoser hvert år.
           </FadeInBlock>
         </Page>
 
@@ -165,30 +165,62 @@ class App extends Component {
           <h2 className="no-bottom">Pårørende ringer</h2>
           <h1 className="no-top no-bottom big">113</h1>
           <FadeInBlock side="left" title="Dronepilot">
-            En dronepilot styrer dronen til rett sted
+            En dronepilot styrer eDrone etter instruks fra AMK og flyr dronen
+            til rett sted.
           </FadeInBlock>
-          <FadeInBlock side="right" title="Responstid på to minutter" />
+          <FadeInBlock side="right" title="Responstid på to minutter">
+            eDrone kan fly med en maks hastighet på 160 km/t og nå ulykkesstedet
+            på rundt to minutter. Vanlig responstid med ambulanser ved overdoser
+            er under åtte minutter i storbyene.
+          </FadeInBlock>
           <FadeInBlock side="left" title="Droneplassering">
-            En drone per apotek er mer enn nok til å dekke hele Trondheim
+            En eDrone per apotek er mer enn nok til å dekke hele Trondheim.
+            Ambulansen drifter og apoteket kan fylle på med nødvendig utstyr og
+            medikamenter.
           </FadeInBlock>
         </Page>
 
         <Page color="white" page="tidslinje-3" refCallback={this.refPage}>
-          <h1>Dronen ankommer ulykkesstedet</h1>
-          <FadeInBlock
-            side="left"
-            title="Kommunikasjonsutstyr og videokamera"
-          />
-          <FadeInBlock side="right" title="Vurdering av pasientens tilstand" />
-          <FadeInBlock
-            side="left"
-            title="Administrering av motgiften NACRAN©"
-          />
-          <FadeInBlock side="right" title="Effekt av motgift" />
+          <h1>eDrone ankommer ulykkesstedet</h1>
+          <FadeInBlock side="left" title="Kommunikasjonsutstyr og videokamera">
+            Dronen er utstyrt med videokamera og kommunikasjonsutstyr som kan
+            overvåke pasienten og veilede de som meldte inn overdosen.
+          </FadeInBlock>
+          <FadeInBlock side="right" title="Vurdering av pasientens tilstand">
+            Dronen har med medisinsk utstyr som EKG- måler og pulsokismeter som
+            gir viktig info om pasientens tilstand til ambulansen.
+          </FadeInBlock>
+          <FadeInBlock side="left" title="Administrering av motgiften NACRAN©">
+            Etter at en overdose har blitt konstatert, får pårørende tilgang til
+            NARCAN © fra dronen. NARCAN © er en nesespray som inneholder en
+            motgift som reverserer effekten av opioider. Nesesprayen er enkel i
+            bruk og kan gis av personer som ikke er helsepersonell.
+          </FadeInBlock>
+          <FadeInBlock side="right" title="Effekt av motgift">
+            Nesesprayen virker raskt og flesteparten vil få tilbake respirasjon
+            etter noen minutter. Hvis ikke kan dronen instruere pårørende til å
+            starte hjerte-lunge redning i påvente av ambulanse evt. gi ny dose
+            NARCAN©.
+          </FadeInBlock>
         </Page>
 
         <Page color="white" page="tidslinje-4" refCallback={this.refPage}>
           <h1>Hva skjer videre med pasienten?</h1>
+          <FadeInBlock side="left">
+            Hva som skjer etter behandling med nesespray avhenger av pasientens
+            tilstand etter endt behandling. Hvis pasienten ikke viser tegn til
+            bedring, sendes en ambulanse til stedet.
+          </FadeInBlock>
+          <FadeInBlock side="right">
+            Pasienter som ikke får tilfredsstillende bedring etter endt
+            behandling blir vurdert av ambulansepersonell og evt. Sendt videre
+            til sykehus for videre behandling.
+          </FadeInBlock>
+          <FadeInBlock side="left">
+            Friske pasienter blir overvåket av dronen i to timer. Alle får
+            tilbud om oppfølging av spesialisthelsetjeneste i etterkant av en
+            overdose.
+          </FadeInBlock>
         </Page>
 
         <Page
